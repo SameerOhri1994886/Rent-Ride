cb063c247c304e8f230ee1234131216ed53a6d5b
Anmolkannu <kanmol1729@gmail.com>
2021-03-21 18:25:39 -0400

    Use Classes

--- UpdatedDatabase.sql
+++ UpdatedDatabase.sql
@@ -3,7 +3,7 @@ USE CarRental;
 CREATE TABLE Customer (
     CustomerID int PRIMARY KEY NOT NULL auto_increment,
     FullName varchar(255) NOT NULL,
-	Username varchar(255) NOT NULL,
+    Username varchar(255) NOT NULL,
     Address varchar(255) NOT NULL,
     Email varchar(70) NOT NULL, 
     Password varchar(70) NOT NULL,
@@ -19,13 +19,13 @@ CREATE TABLE CustomerProfile (
     Email varchar(70) NOT NULL, 
     DrivingLicenseNo varchar(16) NOT NULL,
     Phone varchar(10) NOT NULL,
-	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
+    FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
 	);
 
 CREATE TABLE Employee (
     EmployeeID int PRIMARY KEY NOT NULL auto_increment ,
     FullName varchar(255) NOT NULL,
-	Username varchar(255) NOT NULL,
+    Username varchar(255) NOT NULL,
     Email varchar(70) NOT NULL, 
     Password varchar(70) NOT NULL
 );
@@ -34,18 +34,18 @@ CREATE TABLE CarInventory (
     CarID int PRIMARY KEY NOT NULL auto_increment,
     EmployeeID int NOT NULL,
     CarBrand varchar(255) NOT NULL,
-	CarModel varchar(255) NOT NULL,
+    CarModel varchar(255) NOT NULL,
     Year int NOT NULL,
     CarType varchar(255) NOT NULL,
-	FOREIGN KEY(EmployeeID) REFERENCES Employee(EmployeeID)
+    FOREIGN KEY(EmployeeID) REFERENCES Employee(EmployeeID)
 );
 
 CREATE TABLE CarReservation (
     BookingID int PRIMARY KEY NOT NULL auto_increment,
     CustomerID int NOT NULL,
     StartDate date NOT NULL,
-	EndDate date NOT NULL,
-	FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
+    EndDate date NOT NULL,
+    FOREIGN KEY(CustomerID) REFERENCES Customer(CustomerID)
   );
 
 INSERT INTO CustomerProfile(CustomerID,FullName,Address,Email,DrivingLicenseNo,Phone)
